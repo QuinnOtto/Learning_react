5d3d6060b918d4332bb99b87ee192f4f1f6bd68f
QuinnOtto <101746@glr.nl>
2024-11-08 20:37:08 +0100

    Initialize React app with basic structure and components

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+export function header() {
+  return (
+    <div>
+      <h1 id='title'>Header</h1>
+    </div>
+  );
+}
+
+export function main() {
+  return (
+    <div>
+      <h2 id='description'>Main</h2>
+    </div>
+  );
+}
